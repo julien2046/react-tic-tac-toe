b9a9dac1d92622654c4b90812e995e62b57483ad
julien2046 <j.banchetti@hotmail.fr>
2018-10-12 15:49:35 -0400

    add game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import Players from './Players';
+import Square from './Square';
+
 
 class Board extends Component {
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
 
   render() {
     const { players } = this.props;
@@ -12,6 +23,24 @@ class Board extends Component {
           <Players players={players} />
         </div>
         <div className="board">
+
+          <div className="board__row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+
+          <div className="board__row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+
+          <div className="board__row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
 
         </div>
       </div>
