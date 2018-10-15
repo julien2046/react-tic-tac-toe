a6e89d08a896c54de5e1ffdd8444720a7313c6d5
julien2046 <j.banchetti@hotmail.fr>
2018-10-15 14:17:35 -0400

    Game score

--- src/components/Players.js
+++ src/components/Players.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Players = (props) => {
   const { players, score1, score2 } = props;
+  const score = [score1, score2];
 
   const playersList = (
     <ul className='players__list'>
       {players.map((player, index) =>
         <li key={index}>
-          {player}
+          {player} : {score[index]}
         </li>
       )}
     </ul>
